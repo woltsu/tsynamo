f9f71f85177b9f8ab7f5725d3eb5ab19565e11e5
sasumaki <sausmakinen@gmail.com>
2024-03-22 12:44:59 +0200

    bundle aws-sdk stuff

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,5 +7,7 @@ export default defineConfig({
   treeshake: true,
   dts: true,
   tsconfig: "./tsconfig.json",
-  sourcemap: true
+  sourcemap: true,
+  // Include these packages to the bundle instead of expecting them to be importable from node_modules
+  noExternal: ["@aws-sdk/client-dynamodb", "@aws-sdk/lib-dynamodb"],
 });
